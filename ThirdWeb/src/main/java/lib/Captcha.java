f185cf8a033c0874af0db433958e74c5356242c7
Maksym94 <Sawjkee@ukr.net>
2016-09-27 11:43:27 +0300

    Bank account
    System, look like a bank account site with abilities to register your
    own account, put and get money from your data using JSP as a view and
    servlets as a controllers. Has a connection to database, so no need to
    wait for updating information after act of put or get money.

---
+++ ThirdWeb/src/main/java/lib/Captcha.java
@@ -0,0 +1,83 @@
+package lib;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Maximka
+ * This class is binded with table "captcha" from database where contains data about captcha,
+ * including name of the captcha photo and text on the photo, which will be compared during
+ * registration of a new user. If table does not exist, it will be created automatically
+ * through hibernate annotations and configuration,  
+ *
+ */
+@Entity
+@Table(name="captcha")
+public class Captcha {
+	
+	@Id
+	@Column(name="id_captcha")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idCaptcha;
+	
+	@Column(name="photo_number")
+	private int photoNumber;
+	
+	@Column(name="text_photo")
+	private String textPhoto;
+	
+	public int getIdCaptcha() {
+		return idCaptcha;
+	}
+	public void setIdCaptcha(int idCaptcha) {
+		this.idCaptcha = idCaptcha;
+	}
+	public int getPhotoNumber() {
+		return photoNumber;
+	}
+	public void setPhotoNumber(int photoNumber) {
+		this.photoNumber = photoNumber;
+	}
+	public String getTextPhoto() {
+		return textPhoto;
+	}
+	public void setTextPhoto(String textPhoto) {
+		this.textPhoto = textPhoto;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idCaptcha;
+		result = prime * result + photoNumber;
+		result = prime * result + ((textPhoto == null) ? 0 : textPhoto.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Captcha other = (Captcha) obj;
+		if (idCaptcha != other.idCaptcha)
+			return false;
+		if (photoNumber != other.photoNumber)
+			return false;
+		if (textPhoto == null) {
+			if (other.textPhoto != null)
+				return false;
+		} else if (!textPhoto.equals(other.textPhoto))
+			return false;
+		return true;
+	}
+	
+
+}
---
+++ ThirdWeb/src/main/java/lib/CountUser.java
@@ -0,0 +1,69 @@
+package lib;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Maximka
+ * CountUser - class which is binded table "total_users" from database, where contains data 
+ * about total number count of users whose already registered on the server. If table doesn't 
+ * exists create it through hibernate annotations and configuration.
+ * 
+ */
+@Entity
+@Table(name="total_users")
+public class CountUser {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	private int count;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + count;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CountUser other = (CountUser) obj;
+		if (count != other.count)
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+}
